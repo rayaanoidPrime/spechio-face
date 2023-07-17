91b6c5389e45d2a68f1ea3f54ff92eb8db34d406
rayaanoidPrime <rayaan4dev@gmail.com>
2023-07-17 11:38:03 +0530

    magic button

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const landingscreen = () => {
               <path d="M12.52.55l-5,5h0L.55,12.51l3,3,12-12Zm-4,6,4-4,1,1-4,4.05ZM2.77,3.18A3.85,3.85,0,0,1,5.32,5.73h0A3.85,3.85,0,0,1,7.87,3.18h0A3.82,3.82,0,0,1,5.32.64h0A3.82,3.82,0,0,1,2.77,3.18ZM8.5,2.55h0A2,2,0,0,1,9.78,1.27h0A1.92,1.92,0,0,1,8.5,0h0A1.88,1.88,0,0,1,7.23,1.27h0A1.92,1.92,0,0,1,8.5,2.55Zm-6.36,0h0A1.92,1.92,0,0,1,3.41,1.27h0A1.88,1.88,0,0,1,2.14,0h0A1.92,1.92,0,0,1,.86,1.27h0A2,2,0,0,1,2.14,2.55ZM14.73,6.22h0a1.94,1.94,0,0,1-1.28,1.27h0a1.94,1.94,0,0,1,1.28,1.27h0A1.9,1.9,0,0,1,16,7.49h0A1.9,1.9,0,0,1,14.73,6.22Z"/>
             </svg>
           </a>
-          <div className='tooltip ml-2 whitespace-nowrap absolute left-full z-10 top-1/2 transform -translate-y-1/2  text-white text-sm px-2 py-1 w-auto rounded pointer-events-none opacity-0 transition-opacity duration-200'>
+          <div className='tooltip ml-2 whitespace-nowrap absolute left-full z-10 top-1/2 transform -translate-y-1/2  text-white font-bold text-sm px-2 py-1 w-auto rounded-xl pointer-events-none opacity-0 transition-opacity duration-200'>
             Scan your face and get customized product recommendations for your skin type!
           </div>
         </div>
